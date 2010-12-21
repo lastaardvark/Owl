e79001ff60c85a0d216f7e4d7c47d8d8d899d4d0
Paul <lastaardvark@pjroberts.com>
2010-12-21 22:17:22 +0000

    Added missing autocomplete listbox

---
+++ gui/autoCompleteListBox.py
@@ -0,0 +1,95 @@
+# coding=utf8
+
+# Inspired by http://bit.ly/ggQQCM
+
+from PyQt4.QtGui import QAbstractItemDelegate, QLineEdit, QListView
+from PyQt4.QtCore import QAbstractListModel, QAbstractTableModel, QEvent, QModelIndex, Qt, QVariant, SIGNAL, SLOT
+
+class AutoCompleteListBox:
+        
+    class _LineEdit(QLineEdit):
+        def __init__(self, *args):
+            QLineEdit.__init__(self, *args)
+            
+        def event(self, event):
+            if (event.type()==QEvent.KeyPress) and (event.key()==Qt.Key_Tab):
+                self.emit(SIGNAL("tabPressed"))
+                return True
+            return QLineEdit.event(self, event)
+    
+    
+    class _ListModel(QAbstractListModel): 
+        def __init__(self, listData, parent=None, *args):         
+            QAbstractTableModel.__init__(self, parent, *args) 
+            self.listData = listData
+     
+        def rowCount(self, parent=QModelIndex()):
+            if not self.listData:
+                return 0
+            return len(self.listData) 
+     
+        def data(self, index, role): 
+            if index.isValid() and role == Qt.DisplayRole:
+                return QVariant(self.listData[index.row()])
+            else: 
+                return QVariant()
+    
+        def replaceList(self, data):
+            self.listData = data
+            self.reset()
+    
+    def __init__(self, window, listData):
+        self.window = window
+        self.listData = listData
+    
+        self.lineEdit = self._LineEdit()
+        self.listBox = QListView()
+        
+        self.listModel = self._ListModel(listData, window)
+        self.listBox.setModel(self.listModel)
+        
+        self.window.connect(self.lineEdit, SIGNAL("textChanged(QString)"), self._OnTextChanged)
+        self.window.connect(self.lineEdit, SIGNAL("tabPressed"), self._OnTabPressed)
+        
+    def _OnTextChanged(self):
+        if str(self.lineEdit.text()) == '':
+            self.listModel.replaceList(self.listData)
+            self.subList = []
+        else:
+            pattern = str(self.lineEdit.text())
+            self.subList = [item for item in self.listData if item.lower().find(pattern.lower()) >= 0]
+            self.listModel.replaceList(self.subList)
+    
+    def _OnTabPressed(self):
+        # There’s one possibility
+        if len(self.subList) == 1:
+            self.lineEdit.setText(self.subList[0])
+
+        # multiple possibilities
+        elif len(self.subList) > 1:
+            match = self.subList.pop(0)
+            for word in self.subList:
+                match = _StringIntersection(word, match)
+            self.lineEdit.setText(match)
+              
+    def getLineEdit(self):
+        return self.lineEdit
+                
+    def getListBox(self):
+        return self.listBox
+    
+    def replaceList(self, newList):
+        self.subList = []
+        self.listData = newList
+        self.listModel.replaceList(newList)
+    
+def _StringIntersection(string1, string2):
+    """ The largest common substring of two strings, anchored at the left. """
+    
+    newlist = []
+    for i, j in zip(string1, string2):
+        if i == j:
+            newlist.append(i)
+        else:
+            break
+    return ''.join(newlist)
